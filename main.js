e56fccdf7eaaf60980883bdffa03805aa4d3b729
BonkeG <bonkegcobo291195@gmail.com>
2021-11-08 11:33:40 +0200

    Create mobile menu

---
+++ main.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    const hamburger = document.querySelector(".menubar");
+    hamburger.addEventListener('click',()=>{
+        const menu=document.querySelector('.mobile-menu');
+        menu.style.display="block";
+    });
+
+    const closeBtn = document.querySelector(".closeBtn");
+    closeBtn.addEventListener('click', ()=>{
+        const menu=document.querySelector('.mobile-menu');
+        menu.style.display="none";
+    })
+}
