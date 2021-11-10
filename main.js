c394fd4c8e1469a3306ac37e51da1721bc25a1e1
BonkeG <bonkegcobo291195@gmail.com>
2021-11-10 14:43:43 +0200

    Link each object with pop up window

--- main.js
+++ main.js
@@ -11,6 +11,56 @@ window.onload = () => {
   const item = document.querySelector('.menuPopUp');
   closeMobileMenu(item);
   
+  const project_1=document.querySelector('.projectBtn');
+  project_1.addEventListener('click',()=>{
+    const container = document.querySelector('.cont');
+    container.classList.toggle('container');
+    const popUp=document.querySelector('.ProjectPopUp');
+    popUp.style.display = 'flex';
+  });
+
+
+  /**Open the mobile objects */
+  const project_2=document.querySelector('.btnProject2-1');
+  openPro(project_2);
+  const project_3=document.querySelector('.btnProject2-2');
+  openPro(project_3);
+  const project_4=document.querySelector('.btnProject2-3');
+  openPro(project_4);
+  const project_5=document.querySelector('.btnProject2-4');
+  openPro(project_5);
+  const project_6=document.querySelector('.btnProject2-5');
+  openPro(project_6);
+  const project_7=document.querySelector('.btnProject2-6');
+  openPro(project_7);
+
+  function openPro(project) {
+    project.addEventListener('click', () =>{
+      const container = document.querySelector('.cont');
+      container.classList.toggle('container');
+      const popUp=document.querySelector('.ProjectPopUp');
+      popUp.style.display = 'flex';
+    });
+
+  }
+
+
+
+  /**Close for project */
+  const close_pro=document.querySelector('.closebtn');
+  close(close_pro);
+
+  //Closer For Project
+  function close(theClose){
+    theClose.addEventListener('click',()=>{
+      const container = document.querySelector('.cont');
+      container.classList.toggle('container');
+      const popUp=document.querySelector('.ProjectPopUp');
+      popUp.style.display = 'none';
+    });
+    
+  }
+
   //Closer for the mobile menu
   function closeMobileMenu(closer){
     closer.addEventListener('click', () => {
@@ -146,7 +196,6 @@ window.onload = () => {
       links: ["https://bonkegcobo.github.io/PortfolioProject/",
       "https://github.com/BonkeGcobo/PortfolioProject"
       ]
-      
     },
     {
       id:7,
@@ -170,4 +219,7 @@ window.onload = () => {
       
     },
   ];
+
+
+  /**Populating the Cards with javaScript */
 };
